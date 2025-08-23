87b3e23f7ef699129d1f437c8cb4cf2aa750ada8
Ayoub Amer <ayouub.ameur@gmail.com>
2025-08-23 17:39:49 +0100

    feat: add providerSource enum

---
+++ orchestra-core/src/providers/mod.rs
@@ -0,0 +1,5 @@
+mod gemini;
+mod types;
+
+/// A trait for all providers to implement.
+pub trait Provider {}
---
+++ orchestra-core/src/providers/types.rs
@@ -0,0 +1,22 @@
+/// This is a list of all provider sources that are supported.
+pub enum ProviderSource {
+    Gemini,
+    OpenAI,
+}
+
+impl ProviderSource {
+    pub fn as_str(&self) -> &str {
+        match self {
+            ProviderSource::Gemini => "gemini",
+            ProviderSource::OpenAI => "openai",
+        }
+    }
+
+    pub fn from_str(s: &str) -> Option<ProviderSource> {
+        match s.to_lowercase().as_str() {
+            "gemini" => Some(ProviderSource::Gemini),
+            "openai" => Some(ProviderSource::OpenAI),
+            _ => None,
+        }
+    }
+}
